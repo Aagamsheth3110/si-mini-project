f37ac00a7a5e2bab24bd2ff38f26a22598b1229c
Aagam Sheth <86261564+Aagamsheth3110@users.noreply.github.com>
2022-07-19 18:49:31 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from customer import views 
 
 urlpatterns = [
-    path('email/', views.email, name='email'),
-    path('email_info/', views.email_info, name='email_info'),
+    path('admin/', admin.site.urls),
+    path('customer/', include('customer.urls'))
+    
 
 ]
